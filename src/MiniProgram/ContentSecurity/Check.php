5d5fbf5683e2305f2220883ff932050002a6ab50
luxiaok <me@luxiaok.com>
2020-03-05 15:10:50 +0800

    Add ContentSecurity class

---
+++ src/MiniProgram/ContentSecurity/Check.php
@@ -0,0 +1,49 @@
+<?php
+namespace EasyWeChat\MiniProgram\ContentSecurity;
+
+use EasyWeChat\Core\AbstractAPI;
+use EasyWeChat\Support\Collection;
+
+class Check extends AbstractAPI
+{
+    const API_CHECK_TEXT = 'https://api.weixin.qq.com/wxa/msg_sec_check';
+    const API_CHECK_IMG = 'https://api.weixin.qq.com/wxa/img_sec_check';
+
+
+    /**
+     * Parse JSON from response and check error.
+     *
+     * @param string $method
+     * @param array  $args
+     *
+     * @return \EasyWeChat\Support\Collection | null
+     *
+     * @throws \EasyWeChat\Core\Exceptions\HttpException
+     */
+    public function parseJSON($method, array $args)
+    {
+        $http = $this->getHttp();
+
+        $contents = $http->parseJSON(call_user_func_array([$http, $method], $args));
+
+        if (empty($contents)) {
+            return null;
+        }
+
+        //$this->checkAndThrow($contents);
+
+        return new Collection($contents);
+    }
+
+    /*
+     * 检测文本
+     * */
+    public function checkText($text)
+    {
+        $params = [
+            'content' => $text,
+        ];
+
+        return $this->parseJSON('json', [self::API_CHECK_TEXT, $params]);
+    }
+}
